fc5670d2ff34acea8b622e92591794187808c12d
Rodrigo <rbravo@cnp.com.ar>
2019-08-19 03:25:40 -0300

    Commit sin relevancia

---
+++ Entidades/ConexionApiGmail.cs
@@ -0,0 +1,10 @@
+﻿using Google.Apis.Gmail.v1;
+
+namespace Entidades
+{
+    public class ConexionApiGmail
+    {
+        static string[] Scopes = { GmailService.Scope.GmailReadonly, GmailService.Scope.GmailSend };
+        static string ApplicationName = "Gmail API .NET Quickstart";
+    }
+}

78b71847f908d8d9e529ea0827ba4328a92dee8e
rodrigoebravo <rbravo@cnp.com.ar>
2019-08-27 14:51:39 -0300

    Desarrollo de nueva conexion

--- Entidades/Conexion.cs
+++ Entidades/Conexion.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Mail;
+using System.Threading;
 
 namespace Entidades
 {
@@ -42,6 +43,66 @@ namespace Entidades
                 throw ex;
             }
         }
+
         #endregion
     }
+	public static class EnvioMailHelper
+	{
+		public static void Enviar(string from, string fromName, string to, string subject, string body, bool isBodyHtml = true, MailPriority mailPriority = MailPriority.Normal, string[] adjuntos = null)
+		{
+			var threadSendMails = new Thread(delegate ()
+			{
+				try
+				{
+					using (var mail = new MailMessage())
+					{
+						// De
+						mail.From = new MailAddress(from, fromName);
+
+						// Para
+						foreach (var address in to.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
+						{
+							if (!string.IsNullOrEmpty(address.Trim()))
+								mail.To.Add(address);
+						}
+
+						// Asunto
+						mail.Subject = subject;
+						mail.SubjectEncoding = System.Text.Encoding.UTF8;
+
+						mail.Priority = mailPriority;
+
+						if (adjuntos != null)
+						{
+							foreach (var adjunto in adjuntos)
+								mail.Attachments.Add(new Attachment(adjunto));
+						}
+
+						// Contenido
+						mail.Body = body;
+						mail.BodyEncoding = System.Text.Encoding.UTF8;
+						mail.IsBodyHtml = isBodyHtml;
+
+						using (var smtpClient = new SmtpClient(Configuracion.Host, Configuracion.Port)
+						{
+							DeliveryMethod = SmtpDeliveryMethod.Network,
+							Credentials = new NetworkCredential(Configuracion.Usr, Configuracion.Pass)
+						})
+						{
+							smtpClient.Send(mail);
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					ex.Message.ToString();
+				}
+			})
+			{
+				IsBackground = true
+			};
+
+			threadSendMails.Start();
+		}
+	}
 }
---
+++ Entidades/Configuracion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+public static class Configuracion
+    {
+        public static int Concurrentes;
+        public static string EmailsEmisor;
+        public static string DestinatariosError;
+        public static string Host;
+        public static int Port;
+        public static string Usr;
+        public static string Pass;
+        public static string PathCarpetaDatos;
+        public static int LotePrestamosPendientes;
+
+        static Configuracion()
+        {
+            LeerConfiguracion();
+
+        }
+        private static void LeerConfiguracion()
+        {
+            PathCarpetaDatos = Path.Combine(Directory.GetCurrentDirectory(), "Datos"); 
+            Concurrentes = int.Parse(System.Configuration.ConfigurationManager.AppSettings["Concurrentes"]);
+            EmailsEmisor = System.Configuration.ConfigurationManager.AppSettings["EmailsEmisor"];
+            DestinatariosError = System.Configuration.ConfigurationManager.AppSettings["DestinatariosMailError"];
+            Host = System.Configuration.ConfigurationManager.AppSettings["Smtp.Host"];
+            Port = int.Parse(System.Configuration.ConfigurationManager.AppSettings["Smtp.Port"]);
+            Usr = System.Configuration.ConfigurationManager.AppSettings["Smtp.Usr"];
+            var ps = System.Configuration.ConfigurationManager.AppSettings["Smtp.Pass"];
+            Pass = ps != string.Empty ? ps : null;
+            LotePrestamosPendientes = int.Parse(System.Configuration.ConfigurationManager.AppSettings["LotePrestamosPendientes"]);
+        }
+    }
+}
